fdc20b9c20fdef4e6fc765bbfc8d9ee87e1d4722
Angela Gyi <48336977+angiegyi@users.noreply.github.com>
2021-08-04 23:41:02 +1000

    created base view and connected database for view all obstacles and fixed database, need to do add feature

---
+++ app/src/main/java/com/example/thrive/NewObstacle.java
@@ -0,0 +1,14 @@
+package com.example.thrive;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NewObstacle extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_obstacle);
+    }
+}
