5f473470bcf039df08bc7e874f1e2ac1f9dc8612
agyi0001 <48336977+angiegyi@users.noreply.github.com>
2021-08-11 08:58:18 +1000

    added stub files for hooks

---
+++ app/src/main/java/com/example/thrive/NewHookActivity.java
@@ -0,0 +1,15 @@
+package com.example.thrive;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NewHookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.obstacles);
+    }
+
+}
