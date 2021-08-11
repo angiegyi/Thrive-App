78834d22d334f53f0b0b82a1ef446347159f327e
hngu0025 <hngu0025@gmail.com>
2021-08-11 19:43:17 +1000

    Added Hooks to the DB
    - added hooks
    - methods for adding and deleting hooks and behaviours available

--- app/src/main/java/com/example/thrive/Database/ThriveRepository.java
+++ app/src/main/java/com/example/thrive/Database/ThriveRepository.java
@@ -8,6 +8,7 @@ import androidx.room.Query;
 
 import com.example.thrive.Database.entities.Category;
 import com.example.thrive.Database.entities.Habit;
+import com.example.thrive.Database.entities.Hook;
 import com.example.thrive.Database.entities.Mood;
 import com.example.thrive.Database.entities.Obstacle;
 import com.example.thrive.Database.entities.Obstacle_value;
@@ -26,6 +27,8 @@ public class ThriveRepository {
     private LiveData<List<Mood>> mAllMoods;
     private LiveData<List<Obstacle_value>> mAllObstacle_values;
     private LiveData<List<Obstacle>> mAllObstacles;
+    private LiveData<List<Hook>> mAllHooks;
+
 
     ThriveRepository(Application application) {
         ThriveDatabase db = ThriveDatabase.getDatabase(application);
@@ -35,58 +38,71 @@ public class ThriveRepository {
         mAllMoods = mThriveDAO.getAllMoods();
         mAllObstacle_values = mThriveDAO.getAllObstacle_values();
         mAllObstacles = mThriveDAO.getAllObstacles();
+        mAllHooks = mThriveDAO.getAllHooks();
     }
 
-    LiveData<List<Value>> getAllValues() {
-        return mAllValues;
-    }
+    /*
+    GET ALL FROM TABLE
+     */
+    LiveData<List<Value>> getAllValues() {return mAllValues;}
+    LiveData<List<Category>> getAllCategories(){return mAllCategories;}
+    LiveData<List<Mood>> getAllMoods(){return mAllMoods;}
+    LiveData<List<Obstacle_value>> getAllObstacle_values(){ return mAllObstacle_values;}
+    LiveData<List<Obstacle>> getAllObstacles(){return mAllObstacles;}
+    LiveData<List<Hook>> getAllHooks(){return mAllHooks;}
 
+    /*
+    INSERT INTO DB
+     */
     void insert(Value value) {
         ThriveDatabase.databaseWriteExecutor.execute(() -> mThriveDAO.addValue(value));
     }
-
+    void addCategory(String category_name){
+        ThriveDatabase.databaseWriteExecutor.execute(() -> mThriveDAO.addCategory(category_name));
+    }
+    void addMood(Mood mood){
+        ThriveDatabase.databaseWriteExecutor.execute(() -> mThriveDAO.addMood(mood));
+    }
+    void insert(Habit habit) {
+        ThriveDatabase.databaseWriteExecutor.execute(() -> mThriveDAO.addHabit(habit));
+    }
+    void insert(Obstacle obstacle){
+        ThriveDatabase.databaseWriteExecutor.execute(() -> mThriveDAO.addObstacle(obstacle));
+    }
+    void insert(Obstacle_value obstacle_value){
+        ThriveDatabase.databaseWriteExecutor.execute(() -> mThriveDAO.addObstacle_value(obstacle_value));
+    }
+    void insert(Hook hook){
+        ThriveDatabase.databaseWriteExecutor.execute(()->mThriveDAO.addHook(hook));
+    }
+    /*
+    DELETE METHODS
+     */
     void deleteAll(){
         ThriveDatabase.databaseWriteExecutor.execute(()->{
             mThriveDAO.deleteAllValues();
         });
     }
-
     void deleteValue(String value){
         ThriveDatabase.databaseWriteExecutor.execute(()->{
             mThriveDAO.deleteValue(value);
         });
     }
 
-    void addCategory(String category_name){
-        ThriveDatabase.databaseWriteExecutor.execute(() -> mThriveDAO.addCategory(category_name));
+    void deleteHook(String hook_behaviour){
+        ThriveDatabase.databaseWriteExecutor.execute(()->{
+            mThriveDAO.deleteHook(hook_behaviour);
+        });
     }
 
-    LiveData<List<Category>> getAllCategories(){return mAllCategories;}
-
-    void addMood(Mood mood){
-        ThriveDatabase.databaseWriteExecutor.execute(() -> mThriveDAO.addMood(mood));
+    void deleteObstacle(String obstacle_name){
+        ThriveDatabase.databaseWriteExecutor.execute(()->{
+            mThriveDAO.deleteObstacle(obstacle_name);
+        });
     }
 
-    LiveData<List<Mood>> getAllMoods(){return mAllMoods;}
+    /*
+    UPDATE METHODS
+     */
 
-    void insert(Obstacle obstacle){
-        ThriveDatabase.databaseWriteExecutor.execute(() -> mThriveDAO.addObstacle(obstacle));
-    }
-
-
-    void insert(Obstacle_value obstacle_value){
-        ThriveDatabase.databaseWriteExecutor.execute(() -> mThriveDAO.addObstacle_value(obstacle_value));
-    }
-
-    LiveData<List<Obstacle_value>> getAllObstacle_values(){
-        return mAllObstacle_values;
-    }
-
-    LiveData<List<Obstacle>> getAllObstacles(){
-        return mAllObstacles;
-    }
-
-    void insert(Habit habit) {
-        ThriveDatabase.databaseWriteExecutor.execute(() -> mThriveDAO.addHabit(habit));
-    }
 }
