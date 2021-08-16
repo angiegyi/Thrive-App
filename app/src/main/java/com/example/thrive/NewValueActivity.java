4c1876a5db79d967ae05f9ec4d9be5035e9f0137
Jessica Lee <jlee0021@student.monash.edu>
2021-08-16 12:23:43 +1000

    Re-implementing button outcome
    Prev commit had errors

--- app/src/main/java/com/example/thrive/NewValueActivity.java
+++ app/src/main/java/com/example/thrive/NewValueActivity.java
@@ -3,6 +3,7 @@ package com.example.thrive;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
@@ -15,24 +16,37 @@ import com.google.android.material.slider.Slider;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 
 public class NewValueActivity extends AppCompatActivity{
+
+    Button createButton;
 
     // Initialisation
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         //Take from new_value xml
         setContentView(R.layout.new_value);
+
+        // Initialise create value button
+        initCreateButton();
+    }
+
+
+    // Initialising create button to  value page
+    private void initCreateButton(){
+        createButton = findViewById(R.id.createValueButton);
+        // If button is clicked, the values activity is shown
+        createButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                //Move to values activity page
+                Intent intent = new Intent(NewValueActivity.this, valuesActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
--- app/src/main/java/com/example/thrive/valuesActivity.java
+++ app/src/main/java/com/example/thrive/valuesActivity.java
@@ -3,14 +3,13 @@ package com.example.thrive;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.thrive.Database.ThriveViewModel;
-import com.example.thrive.Database.entities.Category;
-import com.example.thrive.Database.entities.Mood;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.List;
@@ -32,7 +31,14 @@ public class valuesActivity extends AppCompatActivity {
     private void initFab(){
         fab = findViewById(R.id.fabNewVal);
         // If fab button is clicked, the add values activity is shown
-        fab.setOnClickListener(view -> startActivity(new Intent(valuesActivity.this, NewValueActivity.class)));
+        fab.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                //Move to values activity page
+                Intent intent = new Intent(valuesActivity.this, NewValueActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
