05856a1dceca48fef4d263a0caa6eb8df8a0fafa
Jessica Lee <jlee0021@student.monash.edu>
2021-08-16 13:20:25 +1000

    Tester list view

--- app/src/main/java/com/example/thrive/valuesActivity.java
+++ app/src/main/java/com/example/thrive/valuesActivity.java
@@ -2,10 +2,12 @@ package com.example.thrive;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.menu.ListMenuItemView;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -16,15 +18,30 @@ import java.util.List;
 
 public class valuesActivity extends AppCompatActivity {
     FloatingActionButton fab;
+    ListView displayList;
+    String[] testList = {"first","second","third","other"};
 
     // Initialisation
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         // Take from values xml
         setContentView(R.layout.values);
+
+        // Initialise list data
+        initList();
+
         // Init floating action button
         initFab();
+    }
+
+    private void initList(){
+        displayList = findViewById(R.id.valuesList);
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.list_item, testList);
+
+        displayList.setAdapter(adapter);
     }
 
     // Initialising action button to new value page
