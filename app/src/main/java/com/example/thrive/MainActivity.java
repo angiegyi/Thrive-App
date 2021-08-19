aa2ed6aa1a588fe3ea368f7f123e83af474d372d
agyi0001 <48336977+angiegyi@users.noreply.github.com>
2021-08-19 19:23:57 +1000

    created sun onboarding page

--- app/src/main/java/com/example/thrive/MainActivity.java
+++ app/src/main/java/com/example/thrive/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.onboarding_sun);
     }
 
     public void toValuesPage(View view){
