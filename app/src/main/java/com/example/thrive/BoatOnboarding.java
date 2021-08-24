465f32c7590e6ccf6d29a3187328b9a83db42371
agyi0001 <48336977+angiegyi@users.noreply.github.com>
2021-08-24 16:32:29 +1000

    added error checking to angies classes + fixed on boarding flow

--- app/src/main/java/com/example/thrive/BoatOnboarding.java
+++ app/src/main/java/com/example/thrive/BoatOnboarding.java
@@ -20,7 +20,7 @@ public class BoatOnboarding extends AppCompatActivity {
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(BoatOnboarding.this, MainActivity.class));
+                startActivity(new Intent(BoatOnboarding.this, HomeScreen.class));
             }
         });
     }
